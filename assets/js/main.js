d36edc60c9432960b85fbfed0c2c143a80f933a7
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-05 13:40:59 +0100

    - Init Commit. - Create a function to get random number.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// ******CAMPO MINATO****************************************
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// ***********************************************************
+
+// Computer must generate 16 random numbers between 1 and 100.
+// Let's first create a function to generate a random number.
+function randomNumbers (min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+console.log(randomNumbers(1, 100));
+
