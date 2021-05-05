6253133307062c828bf305e11314cf0dc8890a53
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-05 14:36:31 +0100

    - Create array to contain userNumber. - Create a For Cycle to repeat the prompt to ask the user for a number. Verify inserting criterias. - While Cycle in the For Cycle to validate the numbers to insert. - Create an IF to verify if the userNumber matches the numbers of bombs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@
 function randomNumbers (min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(randomNumbers(1, 100));
 // Let's create an array to contain the 16 random numbers
 var bombs = [];
 
@@ -21,10 +20,42 @@ var bombs = [];
 // Numbers must be all different.
 while (bombs.length < 16){
     var pcNumber = randomNumbers(1, 100)
-
+    // Let's verify all numbers in the array are different and if so push them into the array.
     if (!bombs.includes(pcNumber)){
         bombs.push(pcNumber)
     }
 
 }
 console.log(bombs);
+
+// Let's ask the user for (100-16) times to insert always a different number till the end, obviously within 1-100 range.
+// Let's create an array to contain the user number.
+var userArray = [];
+
+// Let's create a FOR Cycle to repeat the request.
+for (i = 1; i <= (100 - 97); i ++){
+    var userNumber = parseInt(prompt("Insert a number between 1 and 100."))
+    
+    // We now need to verify through a WHILE cycle if the number inserted respects all prompt criterias.
+    while (userNumber < 1 || userNumber > 100 || isNaN(userNumber)){
+        alert("You must insert a valid number to play!")
+        userNumber = parseInt(prompt("Insert a number between 1 and 100."))
+    }
+
+    // We also need to verify if the numbers inserted match the numbers contained in the bombs array.
+    if (!bombs.includes(userNumber)){
+        if(bombs.includes(userNumber)){
+            alert("You picked a bomb! You Lost!");
+            break;
+        }
+        userArray.push(userNumber);
+        console.log(userArray);
+
+    } else {
+        alert("You already inserted this number!");
+        i--;
+    }
+
+}
+console.log(i);
+
